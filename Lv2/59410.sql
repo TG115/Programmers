a6d503f5ac8b9353a34e32447730d2ef0cb5ee8f
TG Kim <rora115@naver.com>
2022-08-30 19:00:08 +0900

    NULL 처리하기

--- Lv2/59410.sql
+++ Lv2/59410.sql
@@ -1,4 +1,4 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59412
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59410
 -- NULL 처리하기
 
 SELECT ANIMAL_TYPE, IF(NAME IS NULL, 'No name', NAME), SEX_UPON_INTAKE
