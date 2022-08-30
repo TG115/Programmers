776a7627bc4eaaf7089cbc5df7999c2f7f3cf1cb
TG Kim <rora115@naver.com>
2022-08-30 18:57:53 +0900

    이름에 el이 들어가는 동물 찾기

--- Lv2/59047.sql
+++ Lv2/59047.sql
@@ -1,9 +1,7 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59041
--- 동명 동물 수 찾기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59047
+-- 이름에 el이 들어가는 동물 찾기
 
-SELECT NAME, COUNT(*)
+SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
-WHERE NAME IS NOT NULL
-GROUP BY NAME
-HAVING COUNT(*) > 1
+WHERE NAME LIKE '%el%' AND ANIMAL_TYPE = 'Dog'
 ORDER BY NAME
