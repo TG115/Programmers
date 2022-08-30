a81e4c49178ccf5a3a05fc63dbeabc35a0fd1a6e
TG Kim <rora115@naver.com>
2022-08-30 19:23:35 +0900

    헤비 유저가 소유한 장소

--- Lv3/77487.sql
+++ Lv3/77487.sql
@@ -1,6 +1,17 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/77487
 -- 헤비 유저가 소유한 장소
 
+-- SELECT A.*
+-- FROM PLACES AS A
+--     JOIN (
+--         SELECT HOST_ID, COUNT(*) AS CNT
+--         FROM PLACES
+--         GROUP BY HOST_ID
+--         HAVING CNT > 1
+--     ) AS B
+--     ON A.HOST_ID = B.HOST_ID
+-- ORDER BY A.ID
+
 SELECT A.*
 FROM PLACES AS A
     JOIN (
