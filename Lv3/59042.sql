36ac80fd1c588fe5d4eb63538b69d353344757d5
TG Kim <rora115@naver.com>
2022-08-31 11:13:17 +0900

    없어진 기록 찾기

---
+++ Lv3/59042.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59042
+-- 없어진 기록 찾기
+
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_INS AS I
+    RIGHT JOIN ANIMAL_OUTS AS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.DATETIME IS NULL
+ORDER BY O.ANIMAL_ID
