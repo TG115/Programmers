ffa748431ae529bbeb594ac90274913a2bd51e4a
TG Kim <rora115@naver.com>
2022-08-31 11:16:16 +0900

    보호소에서 중성화한 동물

---
+++ Lv4/59045.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59045
+-- 보호소에서 중성화한 동물
+
+SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
+FROM ANIMAL_INS AS I
+    LEFT JOIN ANIMAL_OUTS AS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.SEX_UPON_INTAKE <> O.SEX_UPON_OUTCOME
+ORDER BY I.ANIMAL_ID
