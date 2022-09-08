b832c54c9d2b45727adf5786268a69e7ee9978cb
TG Kim <rora115@naver.com>
2022-09-08 10:28:27 +0900

    가운데 글자 가져오기

--- Lv1/12903.js
+++ Lv1/12903.js
@@ -1,5 +1,5 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12903
-// 가운데 글자 가져오기
+// 가운데 글자 가져오기 
 
 function solution(s) {
     return s.length%2?s.substr(s.length/2,1):s.substr(s.length/2-1,2);
