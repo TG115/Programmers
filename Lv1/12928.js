9f4e35f854ea75c9013978f00f613ab36f1b14e5
TG Kim <rora115@naver.com>
2022-09-08 10:26:48 +0900

    약수의 합

--- Lv1/12928.js
+++ Lv1/12928.js
@@ -1,5 +1,5 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12928
-// 약수의 합
+// 약수의 합 
 
 function solution(n, sum=0, x=n) {
     return x>0?(n%x?solution(n,sum,--x):solution(n,sum+x,--x)):sum;
