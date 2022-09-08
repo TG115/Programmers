9009b52cd465420a5a512c33b3dcd0cefde33097
TG Kim <rora115@naver.com>
2022-09-08 10:25:04 +0900

    평균 구하기

--- Lv1/12944.js
+++ Lv1/12944.js
@@ -1,5 +1,5 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12944
-// 평균 구하기
+// 평균 구하기 
 
 function solution(arr, sum=0, i=0) {
     return i<arr.length?solution(arr,sum+arr[i],++i):sum/arr.length;
