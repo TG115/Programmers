b92fcc3802ecaab7f90b766773b8f31033e0648b
TG Kim <rora115@naver.com>
2022-10-19 17:25:36 +0900

    각도기

---
+++ Lv0/120829.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/120829
+// 각도기
+
+function solution(angle) {
+    if (angle < 90) return 1;
+    if (angle == 90) return 2;
+    if (angle < 180) return 3;
+    if (angle == 180) return 4;
+}
