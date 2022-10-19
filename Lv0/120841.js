0f8d7873483694f10447df09acc3dcdcc75bf8aa
TG Kim <rora115@naver.com>
2022-10-19 17:31:20 +0900

    점의 위치 구하기

---
+++ Lv0/120841.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/120841
+// 점의 위치 구하기
+
+function solution(dot) {
+    if (dot[0]>0&&dot[1]>0) return 1;
+    if (dot[0]<0&&dot[1]>0) return 2;
+    if (dot[0]<0&&dot[1]<0) return 3;
+    if (dot[0]>0&&dot[1]<0) return 4;
+}
