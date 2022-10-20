287d9332853d226a5fb702968076f15d96875494
TG Kim <rora115@naver.com>
2022-10-20 10:50:08 +0900

    카테고리 별 상품 개수 구하기

--- Lv1/131529.sql
+++ Lv1/131529.sql
@@ -1,4 +1,4 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/131115
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131529
 -- 카테고리 별 상품 개수 구하기
 
 SELECT LEFT(PRODUCT_CODE, 2) AS CATEGORY, COUNT(*) AS PRODUCTS
